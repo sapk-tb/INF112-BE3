b9cea4cc0108b9c8cca56df0a4d56e54413b2ce8
Antoine GIRARD <sapk@sapk.fr>
2015-05-19 15:59:35 +0200

    Test de charge et de timing fini

--- src/test/TestsChargeAndTiming.java
+++ src/test/TestsChargeAndTiming.java
@@ -33,7 +33,17 @@ public class TestsChargeAndTiming {
 		nbErreurs += addNBook(sn, 2500, 2000, "1.2");
 		nbTests++;
 		nbErreurs += addNFilm(sn, 2500, 2000, "1.3");
-		// TODO add moyenne des opération
+
+		nbTests++;
+		System.out.println("Memory used : "
+				+ Runtime.getRuntime().totalMemory() / 1024 / 1024 + "Mo");
+		if (Runtime.getRuntime().totalMemory() / 1024 / 1024 / 1024  > 100 + 10 * (sn
+				.nbMembers() + sn.nbBooks() + sn.nbFilms()) / 1000) {
+			nbErreurs++;
+			System.out.println("Use too much memory !");
+
+		}
+
 		// ce n'est pas du test, mais cela peut "rassurer"...
 		System.out.println(sn);
 		// bilan du test de ReviewOpinion
@@ -52,14 +62,40 @@ public class TestsChargeAndTiming {
 	private static int addNMember(SocialNetwork sn, int nb_user, int max_ms_op,
 			String idTest) {
 		int nbMembers = sn.nbMembers();
+		long max_ns_op = max_ms_op * 1000 * 1000;
+		long[] timings = new long[nb_user];
 		try {
 			for (int i = 1; i <= nb_user; i++) {
-				// TODO add timing
+				long startTime = System.nanoTime();
 				sn.addMember("Utilisateur_" + i, "password_" + i,
 						"Description de l'utitilisateur n°" + i);
+				long endTime = System.nanoTime();
+				timings[i - 1] = (endTime - startTime);
+				if (timings[i - 1] > max_ns_op) {
+					System.out
+							.println("Test "
+									+ idTest
+									+ " : L'operation a pris plus de temps que le temps maximum. "
+									+ timings[i - 1] / (1000 * 1000) + "ms");
+					return 1;
+				}
 			}
-			if (sn.nbMembers() != nbMembers + nb_user){
-				System.out.println("Test " + idTest + " : les utilisateurs semblent avoir été ajoutés mais le nb de membres final est invalide.");
+			// Calcul du temps moyens de toutes les opréations
+			long tot = 0;
+			for (int j = 0; j < timings.length; j++) {
+				tot += timings[j];
+			}
+			System.out.println("Temps total pour l'ajout de " + nb_user
+					+ " membre(s) : " + tot / (1000 * 1000) + "ms");
+			System.out.println("Soit une moyenne de : "
+					+ (tot / timings.length) / (1000) + "us");
+
+			// On vérifie que l'on a le bon nombre de membre
+			if (sn.nbMembers() != nbMembers + nb_user) {
+				System.out
+						.println("Test "
+								+ idTest
+								+ " : les utilisateurs semblent avoir été ajoutés mais le nb de membres final est invalide.");
 				return 1;
 			}
 			return 0;
@@ -70,20 +106,50 @@ public class TestsChargeAndTiming {
 			return 1;
 		}
 	}
+
 	private static int addNBook(SocialNetwork sn, int nb_book, int max_ms_op,
 			String idTest) {
 		int nbBooks = sn.nbBooks();
 		int nbMembers = sn.nbMembers();
+		long max_ns_op = max_ms_op * 1000 * 1000;
+		long[] timings = new long[nb_book];
 		try {
 			for (int i = 1; i <= nb_book; i++) {
-				// TODO add timing
-				int rnd = (int)(Math.random() * nbMembers) + 1;
-	            sn.addItemBook("Utilisateur_" + rnd, "password_" + rnd, "Livre "+i, "Genre du livre "+i, "Auteur"+i, 10+i);
+				long startTime = System.nanoTime();
+				int rnd = (int) (Math.random() * nbMembers) + 1;
+				sn.addItemBook("Utilisateur_" + rnd, "password_" + rnd,
+						"Livre " + i, "Genre du livre " + i, "Auteur" + i,
+						10 + i);
+				long endTime = System.nanoTime();
+				timings[i - 1] = (endTime - startTime);
+				if (timings[i - 1] > max_ns_op) {
+					System.out
+							.println("Test "
+									+ idTest
+									+ " : L'operation a pris plus de temps que le temps maximum. "
+									+ timings[i - 1] / (1000 * 1000) + "ms");
+					return 1;
+				}
 			}
-			if (sn.nbBooks() != nbBooks + nb_book){
-				System.out.println("Test " + idTest + " : les books semblent avoir été ajoutés mais le nb de books final est invalide.");
+			// Calcul du temps moyens de toutes les opréations
+			long tot = 0;
+			for (int j = 0; j < timings.length; j++) {
+				tot += timings[j];
+			}
+			System.out.println("Temps total pour l'ajout de " + nb_book
+					+ " book(s) : " + tot / (1000 * 1000) + "ms");
+			System.out.println("Soit une moyenne de : "
+					+ (tot / timings.length) / (1000) + "us");
+
+			// On vérifie que l'on a le bon nombre de livre
+			if (sn.nbBooks() != nbBooks + nb_book) {
+				System.out
+						.println("Test "
+								+ idTest
+								+ " : les books semblent avoir été ajoutés mais le nb de books final est invalide.");
 				return 1;
 			}
+
 			return 0;
 		} catch (Exception e) {
 			System.out.println("Test " + idTest + " : exception non prévue. "
@@ -92,18 +158,48 @@ public class TestsChargeAndTiming {
 			return 1;
 		}
 	}
+
 	private static int addNFilm(SocialNetwork sn, int nb_film, int max_ms_op,
 			String idTest) {
 		int nbFilms = sn.nbFilms();
 		int nbMembers = sn.nbMembers();
+		long max_ns_op = max_ms_op * 1000 * 1000;
+		long[] timings = new long[nb_film];
+
 		try {
 			for (int i = 1; i <= nb_film; i++) {
-				// TODO add timing
-				int rnd = (int)(Math.random() * nbMembers) + 1;
-	            sn.addItemFilm("Utilisateur_" + rnd, "password_" + rnd, "Film "+i, "Genre du film "+i, "Realisateur"+i, "Scenariste"+i, 10+i);
+				long startTime = System.nanoTime();
+				int rnd = (int) (Math.random() * nbMembers) + 1;
+				sn.addItemFilm("Utilisateur_" + rnd, "password_" + rnd, "Film "
+						+ i, "Genre du film " + i, "Realisateur" + i,
+						"Scenariste" + i, 10 + i);
+				long endTime = System.nanoTime();
+				timings[i - 1] = (endTime - startTime);
+				if (timings[i - 1] > max_ns_op) {
+					System.out
+							.println("Test "
+									+ idTest
+									+ " : L'operation a pris plus de temps que le temps maximum. "
+									+ timings[i - 1] / (1000 * 1000) + "ms");
+					return 1;
+				}
 			}
-			if (sn.nbFilms() != nbFilms + nb_film){
-				System.out.println("Test " + idTest + " : les films semblent avoir été ajoutés mais le nb de films final est invalide.");
+			// Calcul du temps moyens de toutes les opréations
+			long tot = 0;
+			for (int j = 0; j < timings.length; j++) {
+				tot += timings[j];
+			}
+			System.out.println("Temps total pour l'ajout de " + nb_film
+					+ " films(s) : " + tot / (1000 * 1000) + "ms");
+			System.out.println("Soit une moyenne de : "
+					+ (tot / timings.length) / (1000) + "us");
+
+			// On vérifie que l'on a le bon nombre de film
+			if (sn.nbFilms() != nbFilms + nb_film) {
+				System.out
+						.println("Test "
+								+ idTest
+								+ " : les films semblent avoir été ajoutés mais le nb de films final est invalide.");
 				return 1;
 			}
 			return 0;
